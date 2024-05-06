1f57936317dcb27b2db8505e41370eca07fb6a28
MitiqueO4 <gennadykriss6@gmail.com>
2024-05-05 20:07:14 -0400

    Navbar
    Changes

--- grnshift/src/App.js
+++ grnshift/src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from "./components/NavBar/navbar";
+import Home from './pages/Home/home';
+import Reports from './pages/Reports/reports';
+import Products from './pages/Products/products';
+import './App.css';
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/reports' element={<Reports />} />
+          <Route path='/products' element={<Products />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
