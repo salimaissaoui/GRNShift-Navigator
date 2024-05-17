d824a55ef354bf1cd2427cc2be6a2049100f7534
MitiqueO4 <gennadykriss6@gmail.com>
2024-05-16 23:38:06 -0400

    Energy Profile Changes
    Energy profile added

--- grnshift/src/pages/Home/home.js
+++ grnshift/src/pages/Home/home.js
@@ -1,41 +1,69 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './home.css';
+import property_icon from '../../assets/property_icon.png';
+import age_icon from '../../assets/age_icon.png';
+import size_icon from '../../assets/size_icon.png';
+import address_icon from '../../assets/address_icon.png';
 
 function Home() {
-  const [propertyType, setPropertyType] = useState('Detached House');
-  const [propertyAge, setPropertyAge] = useState(30);
-  const [propertySize, setPropertySize] = useState(1500);
-  const [address, setAddress] = useState('15 Arakeen Drive');
+    return (
+        <div className="container">
+            <div className="header">Energy Profile</div>
+            <div className="sub-header">Create your energy profile in just a few minutes!</div>
+            <div className="tab-container">
+                <div className="tab active">Property Information</div>
+                <div className="tab">Energy Usage</div>
+                <div className="tab">Preferences</div>
+            </div>
+            <div className="form">
+                <div className="form-group">
+                    <label>Property Type:</label>
+                    <div className="input-container">
+                        <img src={property_icon} alt="property" />
+                        <input 
+                            className="input" 
+                            placeholder="Detached House"
+                        />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label>Property Age:</label>
+                    <div className="input-container">
+                        <img src={age_icon} alt="age" />
+                        <input 
+                            className="input" 
+                            type="number" 
+                            placeholder="30 Years"
+                        /> 
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label>Property Size:</label>
+                    <div className="input-container">
+                        <img src={size_icon} alt="size" />
+                        <input 
+                            className="input" 
+                            type="number" 
+                            placeholder="1500 Square Feet"
+                        /> 
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label>Address:</label>
+                    <div className="input-container">
+                        <img src={address_icon} alt="address" />
+                        <input 
+                            className="input" 
+                            placeholder="15 Arakeen Drive"
+                        />
+                    </div>
+                </div>
+                <div className="button-container">
+                    <button className="next-button">Next</button>
+                </div>
+            </div>
+        </div>
+    );
+}
 
-  return (
-      <div className="container">
-          <div className="header">Energy Profile</div>
-          <div className="sub-header">Create your energy profile in just a few minutes!</div>
-          <div className="tab-container">
-              <div className="tab active">Property Information</div>
-              <div className="tab">Energy Usage</div>
-              <div className="tab">Preferences</div>
-          </div>
-          <div className="form">
-              <div className="form-group">
-                  <label>Property Type:</label>
-                  <div className="input">{propertyType}</div>
-              </div>
-              <div className="form-group">
-                  <label>Property Age:</label>
-                  <div className="input">{propertyAge} Years</div>
-              </div>
-              <div className="form-group">
-                  <label>Property Size:</label>
-                  <div className="input">{propertySize} Square Feet</div>
-              </div>
-              <div className="form-group">
-                  <label>Address:</label>
-                  <div className="input">{address}</div>
-              </div>
-              <button className="next-button">Next</button>
-          </div>
-      </div>
-  );
-};
 export default Home;
